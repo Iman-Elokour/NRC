6fc4fca17429f26d6f8996f55f284871218b5378
Iman Elokour <48621641+Iman-Elokour@users.noreply.github.com>
2022-07-29 16:37:48 +0300

    init

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -4,9 +4,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=sql11.freemysqlhosting.net;dbname=sql11509610',
+            'username' => 'sql11509610',
+            'password' => 'Uw8tXLQW8u',
             'charset' => 'utf8',
         ],
         'mailer' => [
